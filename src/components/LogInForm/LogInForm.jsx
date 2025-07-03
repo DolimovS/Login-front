12c923e5c6239d6f25c59bffbeda06e0b71b1535
DolimovS <samandardolimov71@gmail.com>
2025-07-03 12:27:01 +0500

    inmutdan maluot olish

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -1,44 +1,62 @@
+import { useState } from "react";
 import "./LogInForm.css"
 
-const LogInForm = ({setShowLogin}) => {
-  const handleGoToLogin = (e)=>{
-        e.preventDefault();
-        setShowLogin(false);
-    }
+const LogInForm = ({ setShowLogin }) => {
+  const handleGoToLogin = (e) => {
+    e.preventDefault();
+    setShowLogin(false);
+  }
+  const [logInIsm, setLogInIsm] = useState('')
+  const [logInFamiliya, setLogInFamiliya] = useState('')
+  const [logInEmail, setLogInEmail] = useState('')
+  const [logInPassword, setLogInPasword] = useState('')
+
+  const handleLogInSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    
-      <form className="loginForm" >
-        <h1>SIGN UP</h1>
-        <p>Enter your credentials to access your account</p>
-        <label htmlFor="">
-          <span>Ism</span>
-          <input
-            type="text"
-          />
-        </label>
-        <label htmlFor="">
-          <span>Familiya</span>
-          <input
-            type="text"
-          />
-        </label>
-        <label htmlFor="">
-          <span>Email</span>
-          <input
-            type="email"
-          />
-        </label>
-        <label htmlFor="">
-          <span>Passpord</span>
-          <input
-            type="password"
-          />
-        </label>
-        <div className="formButtom">
-        <button  onClick={handleGoToLogin} className="loginLink" >Log In</button>
-          <button className="submit_btn">Submit</button> 
-        </div>
-      </form>
+
+    <form className="loginForm" onSubmit={handleLogInSubmit} >
+      <h1>SIGN UP</h1>
+      <p>Enter your credentials to access your account</p>
+      <label htmlFor="">
+        <span>Ism</span>
+        <input
+          type="text"
+          value={logInIsm}
+          onChange={(e) => setLogInIsm(e.target.value)}
+        />
+      </label>
+      <label htmlFor="">
+        <span>Familiya</span>
+        <input
+          type="text"
+          value={logInFamiliya}
+          onChange={(e) => setLogInFamiliya(e.target.value)}
+        />
+      </label>
+      <label htmlFor="">
+        <span>Email</span>
+        <input
+          type="email"
+          value={logInEmail}
+          onChange={(e) => setLogInEmail(e.target.value)}
+        />
+      </label>
+      <label htmlFor="">
+        <span>Passpord</span>
+        <input
+          type="password"
+          value={logInPassword}
+          onChange={(e) => setLogInPasword(e.target.value)}
+        />
+      </label>
+      <div className="formButtom">
+        <button onClick={handleGoToLogin} className="loginLink" >Log In</button>
+        <button className="submit_btn">Submit</button>
+      </div>
+    </form>
   )
 }
 
--- src/components/SignUP/SignUp.jsx
+++ src/components/SignUP/SignUp.jsx
@@ -1,26 +1,43 @@
+import { useState } from "react";
 import "../LogInForm/LogInForm.css"
 
 
 const SignUp = ({setShowLogin}) => {
+    const [singUpEmail,setSingUpEmail]=useState('')
+    const [singUpPassword,setsingUpPassword]=useState('')
+    // const []=useState([])
 
     const handleGoToLogin = (e)=>{
         e.preventDefault();
         setShowLogin(true);
     }
+    const handleSignUpSubmit= (e)=>{
+        e.preventDefault()
+        console.log("Email",singUpEmail);
+        console.log("password",singUpPassword);
+        setSingUpEmail('')
+        setsingUpPassword('')
+    }
+
+
     return (
-        <form className="loginForm" >
+        <form className="loginForm" onSubmit={handleSignUpSubmit} >
             <h1>LONG IN</h1>
             <p>Enter your credentials to access your account</p>
             <label >
                 <span>Email</span>
                 <input
                     type="email"
+                    value={singUpEmail}
+                    onChange={(e)=>setSingUpEmail(e.target.value)}
                 />
             </label>
             <label >
                 <span>Password</span>
                 <input
                     type="password"
+                    value={singUpPassword}
+                    onChange={(e)=>setsingUpPassword(e.target.value)}
                 />
             </label>
             <div className="formButtom">
